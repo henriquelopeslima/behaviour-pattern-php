253bf6684ad8f2ae4268e6e291e1468124f776f8
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2021-08-30 01:00:37 -0300

    Add attribute state in budget

--- src/Budget.php
+++ src/Budget.php
@@ -5,4 +5,25 @@ namespace App\BehaviourPattern;
 class Budget {
     public int $amountItems;
     public float $value;
+    public string $stateCurrent;
+
+    public function applyDiscountExtra()
+    {
+        $this->value -= $this->calculateDiscountExtra();
+    }
+
+    public function calculateDiscountExtra(): float
+    {
+        if ($this->stateCurrent == 'ON_APPROVAL') {
+            return $this->value * 0.05;
+        }
+
+        if ($this->stateCurrent == 'APPROVED') {
+            return $this->value * 0.02;
+        }
+
+        throw  new \DomainException(
+            'Approved and finalized budgets cannot receive a discount'
+        );
+    }
 }
